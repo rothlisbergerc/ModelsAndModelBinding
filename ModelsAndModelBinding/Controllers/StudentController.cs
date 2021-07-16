425d2dd0583962122c40f0d2cf24146c5bb54b72
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-16 11:41:23 -0700

    Created and populated Student object within Add(IFormCollection form) method

--- ModelsAndModelBinding/Controllers/StudentController.cs
+++ ModelsAndModelBinding/Controllers/StudentController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ModelsAndModelBinding.Models;
 
 namespace ModelsAndModelBinding.Controllers
 {
@@ -18,6 +19,13 @@ namespace ModelsAndModelBinding.Controllers
         [HttpPost] // Call this Add() method after user submits (posts) form data
         public IActionResult Add(IFormCollection form)
         {
+            Student stu = new Student();
+            // populate each property:
+            stu.FullName = form["full_name"];
+            stu.DateOfBirth = Convert.ToDateTime(form["dob"]);
+            stu.PhoneNumber = form["home_phone"];
+            stu.EmailAddress = form["email"];
+
             return View();
         }
     }
