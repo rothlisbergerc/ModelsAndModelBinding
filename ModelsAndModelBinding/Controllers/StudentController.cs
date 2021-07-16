41cf48bcbd8fb2a7bcd85293b91a8bd08c68f4c1
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-16 10:47:41 -0700

    Created StudentController

---
+++ ModelsAndModelBinding/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelsAndModelBinding.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
