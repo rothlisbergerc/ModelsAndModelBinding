09736766d72260f2a43b68c51eda8dd85b4f49d7
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-16 11:32:20 -0700

    specified HttpGet and HttpPost on Add methods

--- ModelsAndModelBinding/Controllers/StudentController.cs
+++ ModelsAndModelBinding/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,14 @@ namespace ModelsAndModelBinding.Controllers
 {
     public class StudentController : Controller
     {
+        [HttpGet] // When user navigates to the URL
         public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost] // Call this Add() method after user submits (posts) form data
+        public IActionResult Add(IFormCollection form)
         {
             return View();
         }
